5cdd3c90bb912740d57a3e58a5fde40c63c8806a
Nick <nickbrimmer@gmail.com>
2020-04-25 01:57:48 -0700

    created memoized selectors for the card dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectCartItems} from '../../redux/selectors/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
@@ -19,8 +20,8 @@ const CartDropdown = ({cartItems}) => (
   </div>
 );
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
