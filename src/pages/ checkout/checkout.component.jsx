a61d51fa0c9d6c1d4e96641441d228ab6b1cc21a
Nick <nickbrimmer@gmail.com>
2020-04-26 15:12:50 -0700

    built out and styled a checkout header component

--- src/pages/ checkout/checkout.component.jsx
+++ src/pages/ checkout/checkout.component.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 import './checkout.styles.scss';
 
-const CheckoutPage = () => <div>CHECKOUT PAGE</div>;
+const CheckoutPage = () => (
+  <>
+    <div className='checkout-page' >
+      <div className='checkout-header' >
+        <span className="header-block">Product</span>
+        <span className="header-block">Description</span>
+        <span className="header-block">Quantity</span>
+        <span className="header-block">Price</span>
+        <span className="header-block">Remove</span>
+      </div>
+    </div>
+  </>
+);
 
 export default CheckoutPage;
 
