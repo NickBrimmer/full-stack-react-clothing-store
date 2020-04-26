ae2f665f867716215872ba02b762d3b03841fff2
Nick <nickbrimmer@gmail.com>
2020-04-26 15:23:40 -0700

    created cart total redux hook that pulls in and adds item prices for the total. That total is then displayed in the checkout component

--- src/pages/ checkout/checkout.component.jsx
+++ src/pages/ checkout/checkout.component.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {selectCartItems, selectCartTotal} from '../../redux/selectors/cart.selectors';
+
 import './checkout.styles.scss';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({cartItems, total}) => (
   <>
     <div className='checkout-page' >
-      <div className='checkout-header' >
+      <section className='checkout-header' >
         <span className="header-block">Product</span>
         <span className="header-block">Description</span>
         <span className="header-block">Quantity</span>
         <span className="header-block">Price</span>
         <span className="header-block">Remove</span>
+      </section>
+      {cartItems.map(cartItem => (
+        cartItem.name
+      ))}
+      <div className='total' >
+        <span>TOTAL: ${total}</span>
       </div>
     </div>
   </>
 );
 
-export default CheckoutPage;
+
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+})
+
+export default connect(mapStateToProps)(CheckoutPage);
 
