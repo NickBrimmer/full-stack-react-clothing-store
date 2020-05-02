e341777615471543e8712dd13fc66c3909b9bfa0
Nick <nickbrimmer@gmail.com>
2020-05-01 18:34:49 -0700

    fixed google authorization error conflicting with components running.

--- src/App.js
+++ src/App.js
@@ -9,32 +9,32 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-// import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/selectors/user.selectors';
 import CheckoutPage from './pages/ checkout/checkout.component';
 
 class App extends React.Component {
-  // unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
-  // componentDidMount() {
-  //   const { setCurrentUser } = this.props;
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
-  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-  //     if (userAuth) {
-  //       const userRef = await createUserProfileDocument(userAuth);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-  //       userRef.onSnapshot(snapShot => {
-  //         setCurrentUser({
-  //           id: snapShot.id,
-  //           ...snapShot.data()
-  //         });
-  //       });
-  //     }
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
 
-  //     setCurrentUser(userAuth);
-  //   });
-  // }
+      setCurrentUser(userAuth);
+    });
+  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
