4d4047694c6de9e96ef8760b32f2f70fa6cc0d24
Nick <nickbrimmer@gmail.com>
2020-05-11 15:28:52 -0700

    converted ShopPage over to class component to prepare shop for pulling state from Firebase

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import './shop.scss';
@@ -9,14 +9,19 @@ import CollectionPage from '../collection/collection.component';
 // match, location and history objects automantically
 // passed down as a prop from app with Route
 
-const ShopPage = ({ match }) => (
+class ShopPage extends Component {
 
-  <div className='shop-page'>
-    <Route exact path={ `${match.path}` } component={ CollectionsOverviewComponent } />
-    <Route exact path={ `${match.path}/:collectionId` } component={ CollectionPage } />
-  </div>
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverviewComponent} />
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </div>
+    )
+  }
 
-);
+};
 
 
 
